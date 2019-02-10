59a1c48baacc5c32b53f32893931696b31c96922
fikrirazor <fikrirazor@outlook.co.id>
2019-02-11 01:57:37 +0700

    Update testbigram2.py

--- tesbigram/testbigram2.py
+++ tesbigram/testbigram2.py
@@ -5,7 +5,25 @@ Created on Sun Feb 10 23:44:06 2019
 @author: Rozan
 """
 
-array = ("saya","makan","nasi","goreng","di","rumah")
+S = [1,2,3,4,5,6]
+A = [1,2,3,4,5]
+B = [2,4,6]
 
-def bigram(token):
-    if  
+s=len(S)
+a=len(A)
+b=len(B)
+iab=len(list(set(A) & set(B))) 
+peluanga = a/s 
+peluangb = b/s
+peluangiab = iab/s
+peluangAdengansyaratB = peluangiab/peluangb
+tespeluang = (peluanga*peluangb)/peluangb
+
+W = ("saya","makan","nasi","goreng","di","rumah")
+
+w = len(W)
+wkurang1 = len(W)-1
+peluangw=w/w
+peluangwkurang1 = wkurang1/w
+peluangwndengansyaratwnkurang1 = (peluangw*peluangwkurang1)/peluangwkurang1
+
