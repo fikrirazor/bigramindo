b3ea5bf1b01ee34345b83c4d44ed0c2331702192
riko bintang purnomoputra <rikobintangpp@gmail.com>
2019-02-12 04:28:05 +0700

    Add files via upload

--- tesbigram/testbigram.py
+++ tesbigram/testbigram.py
@@ -1,9 +1,9 @@
-from nltk.util import ngrams
-from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.collocations import *
+from collections import OrderedDict
+import heapq 
 
-
+"""  MODEL BIGRAM """ 
 data = ["i","dont","like","green","and","ham","sam","i","am",'i','am','sam']
+testset = 'sam','am','hate'
 listOfBigrams = []
 bigramCounts = {}
 unigramCounts = {}
@@ -24,13 +24,65 @@ for i in range(len(data)):
 		unigramCounts[data[i]] = 1
 
 
-
+  
+"""  HITUNG PELUANG BIGRAM DAN UNIGRAM """ 
+valueprob = OrderedDict()
 listOfProb = {}
 for bigram in listOfBigrams:
-		word1 = bigram[0]
-		word2 = bigram[1]
-		listOfProb[bigram] = (bigramCounts.get(bigram))/(unigramCounts.get(word1))
+    word1 = bigram[0]
+    word2 = bigram[1]
+    listOfProb[bigram] = (bigramCounts.get(bigram))/(unigramCounts.get(word1))  
+    valueprob[word1+" "+word2] = listOfProb.get(bigram)
+       
+uniprob = {}
+for unigram in data:
+    uniprob[unigram] = 1/ unigramCounts.get(unigram)
+    
+"""  ADD ONE SMOTHING """ 
+def addOneSmothing(listOfBigrams, unigramCounts, bigramCounts):
+    listOfProb = {}
+    cStar = {}
+    for bigram in listOfBigrams:
+        word1 = bigram[0]
+        listOfProb[bigram] = (bigramCounts.get(bigram) + 1)/(unigramCounts.get(word1) + len(unigramCounts))
+        cStar[bigram] = (bigramCounts[bigram] + 1) * unigramCounts[word1] / (unigramCounts[word1] + len(unigramCounts))
+        return listOfProb, cStar
+      
+#addOneSmothing(listOfBigrams, unigramCounts, bigramCounts)
+    
+"""  PERPLEXITY """  
+perplexity = 1
+N = 0
 
-print(listOfProb)
+for i in testset:
+    if i in uniprob:
+        N += 1
+        perplexity = perplexity * (1/uniprob[i])
+        
+perplexity = pow(perplexity, 1/float(N))
+print(perplexity)
 
 
+
+"""  PREDIKSI KATA YANG 	AKAN MUNCUL"""
+
+matchedBigrams = []
+checkForThisBigram = 'i'
+topDict = {}  
+for bigram in listOfBigrams:
+    if checkForThisBigram == bigram[0]:
+            matchedBigrams.append(bigram[0]+" "+bigram[1])
+         
+            
+print(matchedBigrams)            
+	
+for singleBigram in matchedBigrams:
+		topDict[singleBigram] = valueprob[singleBigram]
+
+topBigrams = heapq.nlargest(5, topDict, key=topDict.get)
+for b in topBigrams:
+		print( b+" : "+str(topDict[b])+"\n")
+
+#print(unigramCounts)    
+#print(uniprob)
+#print(listOfBigrams)
